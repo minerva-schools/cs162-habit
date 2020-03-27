f7c3e94e43c68949b430230a31727f71bb7b33d8
Tom Kremer <tom.kremer@minerva.kgi.edu>
2020-03-27 14:36:17 +0300

    using flask-login (added to req file), added basic user functionality: signup-login-logout, hash passwords, force login for 'dashboard'.

--- web/serve.py
+++ web/serve.py
@@ -1,23 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
 import os
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'verysecretkey'
+
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
-
-class User(db.Model):
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
+    username = db.Column(db.String(200), unique=True) #add a way to ensure input is unique
+    password = db.Column(db.String(200)) # hashed upon the creation of User object from signup
+    
+    def __repr__(self):
+        return "<User(id={}, username={}, password={})>".format(self.id, self.username, self.password)
 
 db.create_all()
-example_user = User(id=1, name="Philip Sterne")
-db.session.merge(example_user)
-db.session.commit()
 
 @app.route('/')
 def index():
@@ -28,5 +36,68 @@ def users():
     users = User.query.all()
     return render_template('users.html', users=users)
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(username=username).first() # check if a user exists
+
+    if user: # if a user is found, try again
+        flash('Username already exists')
+        return redirect(url_for('signup'))
+
+    # create new user with the form data
+    new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+    
+@app.route('/login', methods=['POST'])
+def login_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(username=username).first()
+
+    # check if user actually exists
+    if not user:
+        flash('This username does not exist')
+        return redirect(url_for('login'))
+    if not check_password_hash(user.password, password):
+        flash('Incorrect password')
+        return redirect(url_for('login'))
+
+    # if the username exists and the password was correct, go to the user's "dashboard"
+    login_user(user, remember=remember)
+    return redirect(url_for('dashboard'))
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', username=current_user.username)
+    
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+    
 if __name__ == '__main__':
     app.run()
