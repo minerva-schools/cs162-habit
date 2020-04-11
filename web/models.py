d4b7be1b7d97232ed3d9b94419818e420e505b88
Zane Sand <frankzanesand@gmail.com>
2020-04-10 20:03:42 -0500

    Initial commit

--- web/models.py
+++ web/models.py
@@ -3,25 +3,14 @@ from flask_login import UserMixin
 
 
 class User(UserMixin,db.Model):
-    '''
-
-    '''
 
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(200), unique=True) #add a way to ensure input is unique
-    password = db.Column(db.String(200)) # hashed upon the creation of User object from signup
-    score = db.Column(db.Integer, default=0)
-
-    def __repr__(self):
-        return "<User(id={}, username={}, password={}, score={})>".format(self.id, self.username, self.password, self.score)
+    username = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(200))
 
 class Habit(db.Model):
-    '''
-	A habit that a particular existing user is attempting to take on.
-	Includes the associated user, textual description, date created, and active/not status indicator.
-    '''
 
     __tablename__ = 'habit'
 
@@ -30,65 +19,16 @@ class Habit(db.Model):
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(500))
     date_created = db.Column(db.DateTime)
+    frequency = db.Column(db.String(100), default='Daily')
     active = db.Column(db.Boolean)
 
-    def __repr__(self):
-
-        return "<Habit(id={}, user_id={}, title={}, description={}, date_created={},active={})>".format(
-                self.id,
-                self.user_id,
-                self.title,
-                self.description,
-                self.date_created.strftime("%Y-%m-%d %H:%M"),
-                self.active)
-
 class Log(db.Model):
-    '''
-    Log entries for user / habit pairs to track success of habit on desired frequency.
-    '''
 
     __tablename__ = 'log'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     habit_id = db.Column(db.Integer, db.ForeignKey('habit.id'))
-    date_logged = db.Column(db.DateTime)
-    log = db.Column(db.String(500))
-
-    def __repr__(self):
-        return "<Log(id={}, user_id={}, habit_id={}, date_logged={}, log={})>".format(
-            self.id,
-            self.user_id,
-            self.habit_id,
-            self.date_logged,
-            self.log
-        )
-
-class Milestone(db.Model):
-    '''
-
-    '''
-
-    __tablename__ = 'milestone'
-
-    id = db.Column(db.Integer, primary_key=True)
-
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    habit_id  = db.Column(db.Integer, db.ForeignKey('habit.id'), nullable=False)
-
-    title = db.Column(db.String(100), nullable=False)
-
-    note = db.Column(db.String(500)) # milestone-specific comments, such as 'vocabulary session'.
-    deadline = db.Column(db.Date, nullable=False) # deadline is mandatory to determine the score 
-    user_succeeded = db.Column(db.Boolean, default=False) # whether the user performed the activity as planned
-
-    def __repr__(self):
-        return "<Milestone={}, habit_id={}, user_id={}, title={}, deadline={}, user_succeeded={})>".format(
-                self.id,
-                self.habit_id,
-                self.user_id,
-                self.title,
-                self.deadline,
-                self.user_succeeded)
+    date = db.Column(db.DateTime)
+    status = db.Column(db.Boolean)
 
 db.create_all()
--- web/serve.py
+++ web/serve.py
@@ -5,16 +5,7 @@ from flask_login import LoginManager, UserMixin, login_user, logout_user, login_
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 from web import app, db, login_manager
-from .models import User, Habit, Log, Milestone
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/users')
-def users():
-    users = User.query.all()
-    return render_template('users.html', users=users)
+from .models import User, Habit, Log
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -47,9 +38,6 @@ def signup():
         db.session.commit()
 
         return redirect(url_for('login'))
-    else:
-        pass
-        #again, HTTP response handler baby???
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -72,179 +60,67 @@ def login():
 
         # if the username exists and the password was correct, go to the user's "dashboard"
         login_user(user, remember=remember)
-        return redirect(url_for('dashboard'))
-    else:
-        pass
-        #is it a good idea to throw in handlers for the other potential request methods? idk if this is handled in the decorator
+        return redirect(url_for('dashboard', date=date.today()))
+
+@app.route('/dashboard/<date>', methods=['GET', 'POST'])
+@login_required
+def dashboard(date):
+    habits = Habit.query.filter_by(user_id=current_user.id).all()
+    
+    if request.method == 'GET':
+        return render_template(
+            'dashboard.html',
+            user=current_user,
+            date=date,
+            habits=habits)
+    if request.method == 'POST':
+        date = datetime.strptime(date, '%Y-%m-%d')
+        if request.form.get('Yesterday'):
+            #decrease day by one
+            yesterday = date - timedelta(days=1)
+            return render_template(
+                'dashboard.html',
+                user=current_user,
+                date=yesterday.strftime('%Y-%m-%d'),
+                habits=habits)
+        elif request.form.get('Tomorrow'):
+            #increase day by one
+            tomorrow = date + timedelta(days=1)
+            return render_template(
+                'dashboard.html',
+                user=current_user,
+                date=tomorrow.strftime('%Y-%m-%d'),
+                habits=habits)
+        else:
+            return render_template(
+                'dashboard.html',
+                user=current_user,
+                date=date,
+                habits=habits)
+
+@app.route('/add_habit', methods=['GET', 'POST'])
+@login_required
+def add_habit():
+    if request.method == 'GET':
+        return render_template('add_habit.html', user=current_user)
+    elif request.method == 'POST':
+        print(request.form.get('title'))
+        habit = Habit(
+            user_id=current_user.id,
+            title=request.form.get('title'),
+            description=request.form.get('description'),
+            frequency=request.form.get('frequency'),
+            active=True
+        )
+        db.session.add(habit)
+        db.session.commit()
+        return redirect(url_for('dashboard', date=date.today()))
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
-
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    habits = Habit.query.filter_by(user_id=current_user.id).all()
-    milestones = Milestone.query.filter_by(user_id=current_user.id).all()
-    return render_template('dashboard.html', user=current_user, habits=habits, milestones=milestones)
-
-@app.route('/dashboard/add', methods=['POST'])
-@login_required
-def add_habit():
-    try:  # trying to implement a transaction here. Not sure if it's the right way?
-        habit = Habit(user_id=current_user.id, title=request.form['habit_name'],
-                      description=request.form['habit_description'],
-                      date_created=datetime.now(), active=0)
-        db.session.add(habit)
-        db.session.flush() #communicates the operations but does not make pemanent - so now habit as its assigned id
-        log_add = Log(user_id=habit.user_id, habit_id = habit.id, date_logged = habit.date_created, log="{}: Habit created".format(habit.date_created)) # add the first row to the habit's log - habit created
-        db.session.add(log_add)
-        db.session.commit()
-        return redirect(url_for('dashboard'))
-    except:
-        db.session.rollback() # would this silence potential errors?
-        return redirect(url_for('dashboard'))
-
-@app.route('/dashboard/start_habit', methods=['GET', 'POST'])
-@login_required
-def start_habit():  # starting a habit = the habit becomes active
-    try:
-        habit_id = request.form.get('habit_id')  # user selects the passive habit they want to start (selects the id)
-        selected_habit = Habit.query.filter_by(user_id=current_user.id, id=habit_id).first() #get the corresponding habit from the db
-        selected_habit.active = 1 # status of the selected habit is changed to 1 (active)
-        log_activate = Log(user_id=selected_habit.user_id, habit_id = selected_habit.id, date_logged = datetime.now(), log="{}: Habit activated".format(datetime.now())) # add a row to the habit's log indicating activation
-        db.session.add(log_activate)
-        current_user.score += 5  # user gains 5 points when starting a habit
-        db.session.commit()
-        return redirect(url_for('dashboard'))
-    except:
-        db.session.rollback()
-        return redirect(url_for('dashboard'))
-        
-@app.route('/dashboard/stop_habit_<int:id>', methods=['GET', 'POST'])
-@login_required
-def stop_habit(id): # user can mark a given habit as "stopped"
-    # in the html, user stops the habit in the habits table on the dashboard, passing the habit id
-    try:
-        habit = Habit.query.filter_by(id=id).first() # find the selected habit in the db
-        habit.active = False # change the status of the habit to stop        
-        log_stop = Log(user_id=habit.user_id, habit_id = habit.id, date_logged = datetime.now(), log="{}: Habit stopped".format(datetime.now())) # log completetion status
-        db.session.add(log_stop)
-        db.session.commit()
-        return redirect(url_for('dashboard'))
-    except:
-        db.session.rollback()
-        return redirect(url_for('dashboard'))
-
-@app.route('/dashboard/add_milestones', methods=['GET', 'POST'])
-@login_required
-def set_milestones(): # function for the user to add milestone to active habits
-    # asks the user info on the milestone to create:
-    # mandatory: which habit it falls under, title and deadline of the milestone
-    # optional: note about the milestone
-    
-    # Input validation (I'm sure there's a more compact way to do it, but this works for now
-    if not request.form['num_milestones'].isnumeric(): # accounts for negative numbers as well
-        flash('The number of milestones must be a positive number')
-        return redirect(url_for('dashboard'))
-    
-    if not request.form['delta'].isnumeric():
-        flash('The interval between milestones must be a non-negative number')
-        return redirect(url_for('dashboard'))
-        
-    if int(request.form['num_milestones']) == 0:
-        flash('The number of milestones must be positive')
-        return redirect(url_for('dashboard'))
-        
-    if int(request.form['delta']) == 0 and int(request.form['num_milestones']) > 1:
-        flash('The interval between milestones must be positive for a recurring milestone')
-        return redirect(url_for('dashboard'))
-    
-    num_milestones = int(request.form['num_milestones'])
-    original_deadline = datetime.strptime(request.form['deadline'], '%Y-%m-%d') # the user-set deadline
-    increment = timedelta(days=int(request.form['delta'])) # an increment to create recurring milestones
-    
-    # veriy that the deadline has not already passed.
-    if original_deadline.date() < datetime.now().date():
-        flash('The deadline cannot be in the past!')
-        return redirect(url_for('dashboard'))
-    
-    try:
-        for i in range(num_milestones):
-            milestone = Milestone(user_id=current_user.id, habit_id=request.form.get('habit_started'), title=request.form['title'], note=request.form['note'], deadline=original_deadline+i*increment) # create a milestone and add i increments to the deadline
-            db.session.add(milestone)
-            log_milestone_added = Log(user_id=current_user.id, habit_id = request.form.get('habit_started'), date_logged = datetime.now(), log="{}: Milestone '{}' set with deadline {}".format(datetime.now(),milestone.title, datetime.strftime(milestone.deadline, '%Y-%m-%d'))) # add a row to the habit's log for setting a milestone
-            db.session.add(log_milestone_added)
-        db.session.commit()
-        return redirect(url_for('dashboard'))
-    
-    except:
-        db.session.rollback()
-        return redirect(url_for('dashboard'))
-
-
-@app.route('/dashboard/complete_milestone_<int:id>', methods=['GET', 'POST'])
-@login_required
-def complete_milestone(id): # user can mark a given milestone as "completed"
-    # in the html, user selects a milestone (by clicking a button that passes the milestone id)
-    try:
-        milestone = Milestone.query.filter_by(id=id).first() # find the selected milestone in the db
-        milestone.user_succeeded = True # change the status of the milestone to completed
-        if milestone.deadline >= date.today(): # if finished on time, user gets 3 points
-            current_user.score += 3
-            status = 'before the deadline!'
-        elif milestone.deadline < date.today(): # if finished late, user gets 1 point
-            current_user.score += 1
-            status = 'after the deadline :('
-        
-        log_milestone_completed = Log(user_id=milestone.user_id, habit_id = milestone.habit_id, date_logged = datetime.now(), log="{}: Milestone '{}' completed {}".format(datetime.now(),milestone.title,status)) # log completetion status
-        db.session.add(log_milestone_completed)
-        db.session.commit()
-        return redirect(url_for('dashboard'))
-    except:
-        db.session.rollback()
-        return redirect(url_for('dashboard'))
-
-@app.route('/dashboard/<habit_id>')
-@login_required
-def dashboard_habit(habit_id):
-    #pull both Milestones and Logs
-    habit = Habit.query.filter_by(id=habit_id, user_id=current_user.id).first()
-    milestones = Milestone.query.filter_by(habit_id=habit_id, user_id=current_user.id).all()
-    logs = Log.query.filter_by(habit_id=habit_id, user_id=current_user.id).all()
-    return render_template('habit.html', habit=habit, milestones=milestones, logs=logs)
-
-@app.route('/dashboard/<habit_id>/edit', methods=['POST'])
-@login_required
-def edit_habit(habit_id):
-    #get habit being edited
-    habit = Habit.query.filter_by(id=habit_id, user_id=current_user.id).first()
-    habit.title = request.form['title']
-    habit.description = request.form['description']
-    db.session.add(habit)
-    db.session.commit()
-    return redirect(url_for('dashboard_habit', habit_id=habit_id))
-
-@app.route('/dashboard/<habit_id>/delete', methods=['POST'])
-@login_required
-def delete_habit(habit_id):
-    try:
-        habit = Habit.query.filter_by(id=habit_id, user_id=current_user.id).first()
-        Milestone.query.filter_by(habit_id=habit.id).delete() #delete all milestones for the habit
-        Log.query.filter_by(habit_id=habit.id).delete() #delete all logs for the habit
-        db.session.delete(habit)
-        db.session.commit()
-        return redirect(url_for('dashboard'))
-    except:
-        db.session.rollback()
-        return redirect(url_for('dashboard'))
-
-@app.shell_context_processor
-def make_shell_context():
-    '''Allows to work with all objects directly in flask shell'''
-    return {'db': db, 'User': User, 'Habit': Habit, 'Milestone': Milestone, 'Log': Log}
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run()
